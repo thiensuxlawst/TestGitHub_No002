9d97b837543ebc4cbfaf5bdd2c119d30edfcb5a6
thiensuxlawst <thiensuxlawst@gmail.com>
2017-04-02 16:13:34 +0700

    vfggbghn

--- app/src/main/java/com/example/administrator/testgithub_no002/MainActivity.java
+++ app/src/main/java/com/example/administrator/testgithub_no002/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
